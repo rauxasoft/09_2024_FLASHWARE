9367d2ce61655b77726bf1936e2720aa8cbbb763
Jordi <rauxasoft@gmail.com>
2024-09-24 11:38:43 +0200

    se crea repositorio para Establecimiento

---
+++ src/main/java/com/sinensia/flashware/backend/integration/repositories/EstablecimientoRepository.java
@@ -0,0 +1,9 @@
+package com.sinensia.flashware.backend.integration.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sinensia.flashware.backend.business.model.Establecimiento;
+
+public interface EstablecimientoRepository extends JpaRepository<Establecimiento, Long> {
+
+}
